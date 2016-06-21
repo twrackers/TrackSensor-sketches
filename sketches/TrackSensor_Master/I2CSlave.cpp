df464b34f63e986c104faf410eb3477cdcc02553
Thomas W Rackers <twrackers@users.noreply.github.com>
2016-06-21 10:47:11 -0600

    Began adding comments to source.

--- sketches/TrackSensor_Master/I2CSlave.cpp
+++ sketches/TrackSensor_Master/I2CSlave.cpp
@@ -5,11 +5,12 @@
 #include "I2CSlave.h"
 #include "SPIMaster.h"
 
+// I2CSlave class - Handles slave end of I2C link to central processor.
 I2CSlave::I2CSlave(
-  const byte addr,
-  CALLBACK1 receive,
-  CALLBACK0 request,
-  SPIMaster& spi
+  const byte addr,      // I2C address of this processor
+  CALLBACK1 receive,    // address of onReceive callback
+  CALLBACK0 request,    // address of onRequest callback
+  SPIMaster& spi        // SPIMaster object
 ) : 
   StateMachine(1, false), 
   m_addr(addr), 
@@ -19,18 +20,22 @@ I2CSlave::I2CSlave(
   m_timeout(0UL),
   m_spi(spi)
 {
+  // Set up callbacks for I2C interface.
   Wire.onReceive(receive);
   Wire.onRequest(request);
 }
 
 void I2CSlave::begin()
 {
+  // Begin I2C interface.
   Wire.begin(m_addr);
 }
 
 void I2CSlave::reqNumNodes()
 {
+  // If not in active mode...
   if (!m_active) {
+    // Number of connected SPI sensor nodes requested.
     m_requested = m_spi.getNumNodes();
     m_request = true;
   }
@@ -38,7 +43,9 @@ void I2CSlave::reqNumNodes()
 
 void I2CSlave::reqNodeSensors(const byte node)
 {
+  // If not in active mode...
   if (!m_active) {
+    // Status of node's sensors is requested.
     m_requested = m_spi.getNodeSensors(node);
     m_request = true;
   }
@@ -46,14 +53,17 @@ void I2CSlave::reqNodeSensors(const byte node)
 
 void I2CSlave::setActive(bool active)
 {
+  // Switch to active mode.
   m_active = active;
 }
 
 byte I2CSlave::getRequested()
 {
+  // Get byte of data requested by I2C master.
   m_timeout = 0UL;
   byte r = m_requested;
   m_request = false;
+  // ???
   if (m_spi.getRingBuffer().isEmpty()) {
     digitalWrite(LOOK_AT_ME, HIGH);
   }
@@ -62,9 +72,11 @@ byte I2CSlave::getRequested()
 
 bool I2CSlave::update()
 {
+  // Return immediately if not time to update.
   if (!StateMachine::update()) {
     return false;
   }
+  // Otherwise...
   switch (m_state) {
     case eIdle:
       if (m_active) {
